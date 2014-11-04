233c590ed12e96ecc88b9901b275fa61f8009b63
unknown <ismop@VW-ECLIPSE-011.pk.amk.root>
2014-11-04 14:32:04 +0200

    New

---
+++ lab_phoneGap.js
@@ -0,0 +1,24 @@
+
+
+
+	
+	function showRat()
+{	
+   document.getElementsByTagName("IMG")[0].setAttribute('src',"rat.jpg");
+}
+
+function showCat()
+{
+  document.getElementsByTagName("IMG")[0].setAttribute('src',"cat.jpg");
+  
+
+}
+	
+	
+	
+	
+	
+
+
+
+
